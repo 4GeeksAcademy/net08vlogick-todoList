c852bf32341404370654d88eaac49089642b0181
wilson isaac castro chavez <148562958+Next08vlogick@users.noreply.github.com>
2024-02-16 20:20:29 +0000

    proyecto casi terminado

--- src/js/component/Input.js
+++ src/js/component/Input.js
@@ -1,41 +1,53 @@
 import React, { useState } from "react";
 
 const Input = () => {
-  const [todos, setTodos] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const deleteTodo = (index) => {
+    return () => {
+      setTodos(todos.filter((_, i) => i !== index));
+    };
+  };
+  const onKey = (event) => {
+    if (event.key === "Enter") {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
   return (
-    <div>
-      <div className="mt-5 container input-group flex-nowrap">
-        <span className="input-group-text Nombre" id="addon-wrapping">
-          Nombre
-        </span>
-
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Ingresa tu nombre"
-          aria-label="Username"
-          aria-describedby="addon-wrapping"
-          onChange={(e) => {
-            console.log(e.target.value);
-            setTodos(e.target.value);
-          }}
-        />
-      </div>
-
-      <div>
-        <div className="d-flex justify-content-center">
-          <button
-            className=" restablecer btn btn-light mt-3"
-            onClick={() => {
-              setTodos("");
+    <div className="mt-5 text-center container">
+      <h1 className="title ">Todo list</h1>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <input
+            value={input}
+            type="text"
+            className="form-control"
+            placeholder="Nueva tarea"
+            aria-label="Username"
+            aria-describedby="addon-wrapping"
+            onKeyDown={onKey}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setInput(e.target.value);
             }}
-          >
-            Restablecer
-          </button>
-        </div>
+          />
+        </li>
+        {todos.map((todo, index) => (
+          <li className="list-group-item" key={index}>
+            {todo}
+            <button
+              type="button"
+              className="btn deleteButton"
+              onClick={deleteTodo(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
 
-        <h1 className="mt-3 d-flex justify-content-center">Mi nombre es:</h1>
-      </div>
+        <li className="tasks list-group-item">{todos.length} Tasks</li>
+      </ul>
     </div>
   );
 };
